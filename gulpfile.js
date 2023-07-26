15735d48bc4cedc57804b53900d8181e1f2b87cf
Rodgath <25994782+Rodgath@users.noreply.github.com>
2023-07-26 19:47:07 +0300

    Fix bugs

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,23 @@ const rename = require('gulp-rename');
 
 const compressJs = () => {
   return src('src/*.js')
+          .pipe(dest('./demo')) // Send original script file to /demo
+          .pipe(dest('./dist')) // Send original script file to /dist
           .pipe(babel({
               presets: ['@babel/env']
           }))
           .pipe(uglify())
           .pipe(rename( { suffix: '.min' } ))
-          .pipe(dest('demo'))
-          .pipe(dest('dist'))
+          .pipe(dest('./demo'))
+          .pipe(dest('./dist'))
 }
 
 const watchTask = () => {
-  watch(['src/*.js', 'demo/*.html'], { events: 'all' }, compressJs)
+  watch(['./src/*.js', './demo/*.html'], { events: 'all' }, compressJs)
 }
 
 task('compress:js', compressJs);
 
 task('watch', watchTask);
+
+exports.default = compressJs;
