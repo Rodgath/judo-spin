14d9ba68defa83dc91de55b4baad7eee0e68133e
Rodgath <25994782+Rodgath@users.noreply.github.com>
2023-07-30 13:19:34 +0300

    Update compressJs task

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,15 @@ const rename = require('gulp-rename');
 
 const compressJs = () => {
   return src('src/*.js')
-          .pipe(dest('./demo/js')) // Send original script file to /demo
-          .pipe(dest('./dist')) // Send original script file to /dist
-          .pipe(babel({
-              presets: ['@babel/env']
-          }))
-          .pipe(uglify())
-          .pipe(rename( { suffix: '.min' } ))
-          .pipe(dest('./demo/js'))
-          .pipe(dest('./dist'))
+        .pipe(dest('./demo/js')) // Send original script file to /demo
+        .pipe(dest('./dist/js')) // Send original script file to /dist
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(rename( { suffix: '.min' } ))
+        .pipe(dest('./demo/js'))
+        .pipe(dest('./dist/js'))
 }
 
 const watchTask = () => {
